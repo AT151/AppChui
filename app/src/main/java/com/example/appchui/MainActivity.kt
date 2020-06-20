733dcbc3c88db7760b98288a04ee2c4f68923f00
The Anh <atran25@jhu.edu>
2020-06-20 18:08:27 +0700

    Added Settings Button Added back button to Settings Activity

--- app/src/main/java/com/example/appchui/MainActivity.kt
+++ app/src/main/java/com/example/appchui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.appchui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.action_buttons, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.getItemId()
+
+        if (id == R.id.settings) {
+            val intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
 
     }
 }
--- app/src/main/java/com/example/appchui/Settings.kt
+++ app/src/main/java/com/example/appchui/Settings.kt
@@ -8,5 +8,15 @@ class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
