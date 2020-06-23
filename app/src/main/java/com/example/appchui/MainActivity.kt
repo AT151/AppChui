983452c3d9b5b5831b4fc7f9fe0c246b9a6798e0
The Anh <atran25@jhu.edu>
2020-06-23 21:17:18 +0700

    Added Settings Button Added back button to Settings Activity

--- app/src/main/java/com/example/appchui/MainActivity.kt
+++ app/src/main/java/com/example/appchui/MainActivity.kt
@@ -12,9 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
-
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
