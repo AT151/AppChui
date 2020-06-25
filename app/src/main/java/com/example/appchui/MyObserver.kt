db757f378434123ec52cabf89dcb05291d71842f
The Anh <atran25@jhu.edu>
2020-06-25 14:56:09 +0700

    Edited life cycle observing thing

--- app/src/main/java/com/example/appchui/MyObserver.kt
+++ app/src/main/java/com/example/appchui/MyObserver.kt
@@ -1,23 +1,26 @@
 package com.example.appchui
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ProcessLifecycleOwner
 
-class ArchLifecycleApp : Application(), LifecycleObserver {
+class MyObserver : Application(), LifecycleObserver {
     override fun onCreate() {
         super.onCreate()
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onAppBackgrounded() {
         //App in background
+        Log.d("wow","LETS GOOOOOO")
+
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onAppForegrounded() {
         // App in foreground
     }
