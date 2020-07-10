289e0c7b60e0692ab67b14b5d1f4ba8763fcdb3d
Quan Pham <qvpham@bu.edu>
2020-07-10 16:41:57 +0700

    set up firebase auth UI

--- app/src/main/java/com/example/appchui/MainActivity.kt
+++ app/src/main/java/com/example/appchui/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.appchui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.appchui.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    val auth = FirebaseAuth.getInstance().currentUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        if (auth == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
+            txt_name.text = auth.displayName
+        }
     }
 }
